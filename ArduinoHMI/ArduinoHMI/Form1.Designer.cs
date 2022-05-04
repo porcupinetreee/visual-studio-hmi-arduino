eb035a365d9b48717e77f10950b00c5721c51d08
TuranYasar <49839246+porcupinetreee@users.noreply.github.com>
2022-05-04 16:40:33 +0300

    Add files via upload

---
+++ ArduinoHMI/ArduinoHMI/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace ArduinoHMI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.aGauge1 = new AGaugeApp.AGauge();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DtrEnable = true;
+            this.serialPort1.RtsEnable = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(363, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Tag = "button1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(363, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Tag = "button2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(363, 113);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Tag = "button3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(480, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Tag = "status1";
+            this.label1.Text = "STATUS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(480, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Tag = "status2";
+            this.label2.Text = "STATUS";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(480, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Tag = "status3";
+            this.label3.Text = "STATUS";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(315, 324);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(215, 39);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "CONNECT TO ARDUINO";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // aGauge1
+            // 
+            this.aGauge1.BaseArcColor = System.Drawing.Color.Gray;
+            this.aGauge1.BaseArcRadius = 80;
+            this.aGauge1.BaseArcStart = 100;
+            this.aGauge1.BaseArcSweep = 200;
+            this.aGauge1.BaseArcWidth = 2;
+            this.aGauge1.Cap_Idx = ((byte)(1));
+            this.aGauge1.CapColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Black,
+        System.Drawing.Color.Black,
+        System.Drawing.Color.Black,
+        System.Drawing.Color.Black,
+        System.Drawing.Color.Black};
+            this.aGauge1.CapPosition = new System.Drawing.Point(60, 60);
+            this.aGauge1.CapsPosition = new System.Drawing.Point[] {
+        new System.Drawing.Point(10, 10),
+        new System.Drawing.Point(60, 60),
+        new System.Drawing.Point(10, 10),
+        new System.Drawing.Point(10, 10),
+        new System.Drawing.Point(10, 10)};
+            this.aGauge1.CapsText = new string[] {
+        "",
+        "TEMPERATURE",
+        "",
+        "",
+        ""};
+            this.aGauge1.CapText = "TEMPERATURE";
+            this.aGauge1.Center = new System.Drawing.Point(100, 100);
+            this.aGauge1.Font = new System.Drawing.Font("RomanT", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.aGauge1.Location = new System.Drawing.Point(114, 55);
+            this.aGauge1.MaxValue = 50F;
+            this.aGauge1.MinValue = -10F;
+            this.aGauge1.Name = "aGauge1";
+            this.aGauge1.NeedleColor1 = AGaugeApp.AGauge.NeedleColorEnum.Gray;
+            this.aGauge1.NeedleColor2 = System.Drawing.Color.DimGray;
+            this.aGauge1.NeedleRadius = 80;
+            this.aGauge1.NeedleType = 0;
+            this.aGauge1.NeedleWidth = 2;
+            this.aGauge1.Range_Idx = ((byte)(3));
+            this.aGauge1.RangeColor = System.Drawing.Color.Red;
+            this.aGauge1.RangeEnabled = true;
+            this.aGauge1.RangeEndValue = 50F;
+            this.aGauge1.RangeInnerRadius = 70;
+            this.aGauge1.RangeOuterRadius = 80;
+            this.aGauge1.RangesColor = new System.Drawing.Color[] {
+        System.Drawing.Color.DeepSkyBlue,
+        System.Drawing.Color.LightSkyBlue,
+        System.Drawing.Color.Lime,
+        System.Drawing.Color.Red,
+        System.Drawing.SystemColors.Control};
+            this.aGauge1.RangesEnabled = new bool[] {
+        true,
+        true,
+        true,
+        true,
+        false};
+            this.aGauge1.RangesEndValue = new float[] {
+        5F,
+        15F,
+        30F,
+        50F,
+        0F};
+            this.aGauge1.RangesInnerRadius = new int[] {
+        70,
+        70,
+        70,
+        70,
+        70};
+            this.aGauge1.RangesOuterRadius = new int[] {
+        80,
+        80,
+        80,
+        80,
+        80};
+            this.aGauge1.RangesStartValue = new float[] {
+        -10F,
+        5F,
+        15F,
+        30F,
+        0F};
+            this.aGauge1.RangeStartValue = 30F;
+            this.aGauge1.ScaleLinesInterColor = System.Drawing.Color.Black;
+            this.aGauge1.ScaleLinesInterInnerRadius = 73;
+            this.aGauge1.ScaleLinesInterOuterRadius = 80;
+            this.aGauge1.ScaleLinesInterWidth = 1;
+            this.aGauge1.ScaleLinesMajorColor = System.Drawing.Color.Black;
+            this.aGauge1.ScaleLinesMajorInnerRadius = 70;
+            this.aGauge1.ScaleLinesMajorOuterRadius = 80;
+            this.aGauge1.ScaleLinesMajorStepValue = 10F;
+            this.aGauge1.ScaleLinesMajorWidth = 2;
+            this.aGauge1.ScaleLinesMinorColor = System.Drawing.Color.Gray;
+            this.aGauge1.ScaleLinesMinorInnerRadius = 75;
+            this.aGauge1.ScaleLinesMinorNumOf = 9;
+            this.aGauge1.ScaleLinesMinorOuterRadius = 80;
+            this.aGauge1.ScaleLinesMinorWidth = 1;
+            this.aGauge1.ScaleNumbersColor = System.Drawing.Color.Black;
+            this.aGauge1.ScaleNumbersFormat = null;
+            this.aGauge1.ScaleNumbersRadius = 95;
+            this.aGauge1.ScaleNumbersRotation = 0;
+            this.aGauge1.ScaleNumbersStartScaleLine = 0;
+            this.aGauge1.ScaleNumbersStepScaleLines = 1;
+            this.aGauge1.Size = new System.Drawing.Size(198, 206);
+            this.aGauge1.TabIndex = 0;
+            this.aGauge1.Text = "aGauge1";
+            this.aGauge1.Value = 0F;
+            this.aGauge1.ValueInRangeChanged += new AGaugeApp.AGauge.ValueInRangeChangedDelegate(this.aGauge1_ValueInRangeChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.aGauge1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AGaugeApp.AGauge aGauge1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ ArduinoHMI/ArduinoHMI/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace ArduinoHMI
+{
+    public partial class Form1 : Form
+    {
+        String[] ports;
+        SerialPort serialport;
+        bool isConnected = false;
+        public Form1()
+        {
+            InitializeComponent();
+            getAvailableComPorts();
+        }
+        
+
+        private void aGauge1_ValueInRangeChanged(object sender, AGaugeApp.AGauge.ValueInRangeChangedEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                connectToArduino();
+            }
+            else
+            {
+                disconnectFromArduino();
+            }
+
+        }
+        private void disconnectFromArduino()
+        {
+            isConnected = false;
+            serialport.Close();
+            button4.Text = "CONNECT";
+            button4.BackColor = Color.Green;
+
+        }
+
+        private void connectToArduino()
+        {
+            try
+            {
+                string selectedPort = ports[0];
+                int baudRate = 9600;
+                serialport = new SerialPort(selectedPort, baudRate, Parity.None, 8, StopBits.One);
+                serialport.Open();
+                button4.Text = "DISCONNECT";
+                button4.BackColor = Color.Red;
+                isConnected = true;
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Bağlanma Hatası");
+                throw;
+            }
+        }
+
+
+        void getAvailableComPorts()
+        {
+            ports = SerialPort.GetPortNames();
+
+        }
+
+        int b1 = 0, b2 = 0, b3 = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            b1++;
+            aGauge1.Value = 40;
+            if(b1 % 2 == 1)
+                label1.BackColor = Color.Green;
+            else
+                label1.BackColor = Color.Red;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            b2++;
+            aGauge1.Value = 30;
+            if (b2 % 2 == 1)
+                label2.BackColor = Color.Green;
+            else
+                label2.BackColor = Color.Red;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            b3++;
+            aGauge1.Value = 10;
+            if (b3 % 2 == 1)
+                label3.BackColor = Color.Green;
+            else
+                label3.BackColor = Color.Red;
+        }
+    }
+}
